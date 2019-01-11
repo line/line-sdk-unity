15c8e750ca4d985d0552ad3a5c592fa42f4feea1
onevcat <wei.wang@linecorp.com>
2019-01-11 18:22:38 +0900

    Add Android and editor interface

---
+++ LINE_SDK_Unity/Assets/LineSDK/Interface/LineSDKInterfaceAndroid.cs
@@ -0,0 +1,15 @@
+#if UNITY_ANDROID
+namespace Line.LineSDK {
+    internal class NativeInterface {
+        internal static void SetupSDK(string channelId, string universalLinkURL) {}
+        internal static void Login(string scope, bool onlyWebLogin, string botPrompt, string identifier) {}
+        internal static void Logout(string identifier) {}
+        internal static void RefreshAccessToken(string identifier) {}
+        internal static void RevokeAccessToken(string identifier) {}
+        internal static void VerifyAccessToken(string identifier) {}
+        internal static void GetProfile(string identifier) {}
+        internal static void GetBotFriendshipStatus(string identifier) {}
+        internal static string GetCurrentAccessToken() { return null; }
+    }
+}
+#endif
