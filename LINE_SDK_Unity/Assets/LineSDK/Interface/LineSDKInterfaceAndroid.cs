d7bda22a6cdec9afcbf96968f010ea87a5e7baee
Daniel Kao <daniel.kao@linecorp.com>
2019-01-18 18:19:39 +0800

    Add LineSDKInterfaceAndroid.cs modifications for login, get profile and current token.

--- LINE_SDK_Unity/Assets/LineSDK/Interface/LineSDKInterfaceAndroid.cs
+++ LINE_SDK_Unity/Assets/LineSDK/Interface/LineSDKInterfaceAndroid.cs
@@ -1,15 +1,60 @@
 #if UNITY_ANDROID
-namespace Line.LineSDK {
-    internal class NativeInterface {
-        internal static void SetupSDK(string channelId, string universalLinkURL) {}
-        internal static void Login(string scope, bool onlyWebLogin, string botPrompt, string identifier) {}
-        internal static void Logout(string identifier) {}
-        internal static void RefreshAccessToken(string identifier) {}
-        internal static void RevokeAccessToken(string identifier) {}
-        internal static void VerifyAccessToken(string identifier) {}
-        internal static void GetProfile(string identifier) {}
-        internal static void GetBotFriendshipStatus(string identifier) {}
-        internal static string GetCurrentAccessToken() { return null; }
+using UnityEngine;
+
+namespace Line.LineSDK
+{
+    internal class NativeInterface
+    {
+        static AndroidJavaObject lineSdkWrapper = new AndroidJavaObject("linesdk.linecorp.com.sdknativemodule.LineSdkWrapper");
+
+        static NativeInterface()
+        {
+            // Ensure the LineSDK instance existing.
+            var _ = LineSDK.Instance;
+        }
+
+        internal static void SetupSDK(string channelId, string universalLinkURL)
+        {
+            object[] parameters = new object[1];
+            parameters[0] = channelId;
+
+            lineSdkWrapper.Call("setupSdk", parameters);
+
+        }
+        internal static void Login(string scope, bool onlyWebLogin, string botPrompt, string identifier)
+        {
+            object[] parameters = new object[4];
+            parameters[0] = identifier;
+            parameters[1] = scope;
+            parameters[2] = onlyWebLogin;
+            parameters[3] = botPrompt;
+
+            lineSdkWrapper.Call("login", parameters);
+        }
+
+        internal static void Logout(string identifier)
+        {
+            object[] parameters = new object[1];
+            parameters[0] = identifier;
+
+            lineSdkWrapper.Call("logout", parameters);
+        }
+
+        internal static void RefreshAccessToken(string identifier) { }
+        internal static void RevokeAccessToken(string identifier) { }
+        internal static void VerifyAccessToken(string identifier) { }
+        internal static void GetProfile(string identifier)
+        {
+            object[] parameters = new object[1];
+            parameters[0] = identifier;
+            lineSdkWrapper.Call("getProfile", parameters);
+        }
+
+        internal static void GetBotFriendshipStatus(string identifier) { }
+        internal static string GetCurrentAccessToken()
+        {
+            return lineSdkWrapper.Call<string>("getCurrentAccessToken");
+        }
     }
 }
 #endif
