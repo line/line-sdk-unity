f1cce7319160af923fb78a97eddd2f9352d76bb1
Daniel Kao <daniel.kao@linecorp.com>
2019-01-24 18:00:33 +0800

    Correctly parse Scope; handle case when profile scope is not defined

--- linesdk-android-unity-wrapper/src/main/java/com/linecorp/linesdk/unitywrapper/model/LoginResultForUnity.kt
+++ linesdk-android-unity-wrapper/src/main/java/com/linecorp/linesdk/unitywrapper/model/LoginResultForUnity.kt
@@ -1,27 +1,29 @@
 package com.linecorp.linesdk.unitywrapper.model
 
+import com.linecorp.linesdk.Scope
 import com.linecorp.linesdk.auth.LineLoginResult
 
 
 data class LoginResultForUnity(
     val accessToken: AccessTokenForUnity,
     val scope: String,
-    val userProfile: UserProfile,
+    val userProfile: UserProfile?,
     val friendshipStatusChanged: Boolean
 ) {
     companion object {
         fun convertLineResult(lineLoginResult: LineLoginResult): LoginResultForUnity? {
             val accessToken = AccessTokenForUnity.convertFromLineLoginResult(lineLoginResult) ?: return null
-            val lineProfile = lineLoginResult.lineProfile ?: return null
-            val scope = lineLoginResult.lineCredential?.scopes?.joinToString(",") {
-                    scope -> scope.code } ?: ""
+            val lineProfile = lineLoginResult.lineProfile?.let {
+                UserProfile.convertLineProfile(it)
+            } ?: null
+            val scope = lineLoginResult.lineCredential?.scopes?.let {
+                Scope.join(it)
+            } ?: ""
             return LoginResultForUnity(
                 accessToken,
                 scope,
-                UserProfile.convertLineProfile(
-                    lineProfile
-                ),
-                lineLoginResult.friendshipStatusChanged
+                lineProfile,
+                lineLoginResult.friendshipStatusChanged ?: false
             )
         }
 
