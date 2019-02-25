2eb696baaa9d12e02590b3f70d33b85240fb657e
onevcat <onevcat@gmail.com>
2019-02-25 20:31:39 +0900

    Use helper class methods

--- LINE_SDK_Unity/Assets/LineSDK/Editor/CocoaPods/CocoaPodsInstalling.cs
+++ LINE_SDK_Unity/Assets/LineSDK/Editor/CocoaPods/CocoaPodsInstalling.cs
@@ -38,9 +38,9 @@ namespace Line.LineSDK.Editor {
             }
         
             // Add usual ruby runtime manager path to process.
-            AddSearchPaths();
+            ShellCommand.AddPossibleRubySearchPaths();
 
-            var podExisting = Run("which", "pod");
+            var podExisting = ShellCommand.Run("which", "pod");
             if (string.IsNullOrEmpty(podExisting)) {
                 var text = @"LINE SDK integrating failed. Building LINE SDK for iOS target requires CocoaPods, but it is not installed. Please run ""sudo gem install cocoapods"" and try again.";
                 UnityEngine.Debug.LogError(text);
@@ -63,7 +63,7 @@ namespace Line.LineSDK.Editor {
             }
 
             Directory.SetCurrentDirectory(pathToBuiltProject);
-            var log = Run("pod", "install");
+            var log = ShellCommand.Run("pod", "install");
             UnityEngine.Debug.Log(log);
             Directory.SetCurrentDirectory(currentDirectory);
 
@@ -79,40 +79,6 @@ namespace Line.LineSDK.Editor {
             project.SetBuildProperty(target, "GCC_PREPROCESSOR_DEFINITIONS", "$(inherited) LINESDK_COCOAPODS=1");
 
             project.WriteToFile(path);
-        }
-
-        static void AddSearchPaths() {
-            AddSearchPath(AppendingHome(".rbenv/shims"));
-            AddSearchPath(AppendingHome(".rvm/scripts/rvm"));
-            AddSearchPath("/usr/local/bin/");
-        }
-
-        static string AppendingHome(string path) {
-            var homePath = System.Environment.GetEnvironmentVariable("HOME");
-            return Path.Combine(homePath, path);
-        }
-
-        static void AddSearchPath(string path) {
-            var name = "PATH";
-            var currentPath = System.Environment.GetEnvironmentVariable(name);
-            var newPath = currentPath + ":" + path;
-            UnityEngine.Debug.Log(newPath);
-            var target = EnvironmentVariableTarget.Process;
-            System.Environment.SetEnvironmentVariable(name, newPath, target);
-        }
-
-        static string Run(string command, string args) {
-            ProcessStartInfo psi = new ProcessStartInfo(); 
-            psi.FileName = command;
-            psi.UseShellExecute = false;
-            psi.RedirectStandardOutput = true;
-            if (args != null) {
-                psi.Arguments = args;
-            }
-            Process p = Process.Start(psi); 
-            string strOutput = p.StandardOutput.ReadToEnd(); 
-            p.WaitForExit(); 
-            return strOutput;
         }
     }
 }
--- LINE_SDK_Unity/Assets/LineSDK/Utils/Helpers.cs
+++ LINE_SDK_Unity/Assets/LineSDK/Utils/Helpers.cs
@@ -22,7 +22,7 @@ namespace Line.LineSDK {
     public static class Helpers {
         internal static bool IsInvalidRuntime(string identifier, RuntimePlatform platform) {
             if (Application.platform != platform) {
-                Debug.LogWarning("[LINE SDK] This RuntimePlatform is not supported. Only iOS and Android is supported.");
+                Debug.LogWarning("[LINE SDK] This RuntimePlatform is not supported. Only iOS and Android devices are supported.");
                 var errorJson = @"{""code"":-1, ""message"":""Platform not supported.""}";
                 var result = CallbackPayload.WrapValue(identifier, errorJson);
                 LineSDK.Instance.OnApiError(result);
